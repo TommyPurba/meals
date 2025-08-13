a5f0a1ecdb253bcc9f119625761ffbef8b56b564
Maruli <tommypurba73@gmail.com>
2025-08-13 16:58:42 +0700

    add file function models (category.dart)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/category.dart';
+import 'package:meals/screen/category.dart';
 
 
 final theme = ThemeData(
