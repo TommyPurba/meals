a323a1e3f2f41456c8563d6e9c22305643e1b517
Maruli <tommypurba73@gmail.com>
2025-08-16 19:49:21 +0700

    add improve the mealdetailscreen

--- lib/screen/meal_detail.dart
+++ lib/screen/meal_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/main.dart';
 import 'package:meals/models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget{
@@ -12,11 +13,58 @@ class MealDetailScreen extends StatelessWidget{
         title: 
                Text(meal.title),
       ),
-      body: Image.network(
-        meal.imageUrl,
-        height: 300,
-        width: double.infinity,
-        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+            meal.imageUrl,
+            height: 300,
+            width: double.infinity,
+            fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 14,),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 14,),
+            for(final ingredients in meal.ingredients)
+               Text(
+                ingredients,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+              ),
+            
+            const SizedBox(height: 24,),
+            Text(
+              'step',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 14,),
+            for(final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 8,
+              ),
+              child: Text(
+                step,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+              ),
+            ),  
+          ],
+        ),
       ),
     );
   }
